bee369086370720e08e78bb8e7ab1aa4e9ab5539
Lemon Juice <stanly.lukmana@gmail.com>
2025-06-26 13:01:50 +0700

    Passwrod Maker

--- Passwrod Maker.py
+++ Passwrod Maker.py
@@ -3,5 +3,5 @@ import random
 passrn = int(input("Enter the length of password that you need :"))
 s="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!@#$%^&*_-:;?+<>"
 
-pass = "".join(random.sample(s, passrn))
-print("Your Password:",pass)
+p = "".join(random.sample(s, passrn))
+print("Your Password:",p)
